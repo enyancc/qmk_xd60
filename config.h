886d5b862c9f6142f4584dcfe3514ff7231204f1
Sergii Naumov <sergii.naumov@siemens.com>
2017-10-25 17:05:09 +0200

    Add ability to switch between layers

--- config.h
+++ config.h
@@ -65,6 +65,8 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 #define RGBLIGHT_HUE_STEP 10
 #define RGBLIGHT_SAT_STEP 17
 #define RGBLIGHT_VAL_STEP 17
+#define TAPPING_TERM 200
+
 
 /* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
 #define LOCKING_SUPPORT_ENABLE
--- keymaps/white64/keymap.c
+++ keymaps/white64/keymap.c
@@ -9,6 +9,10 @@ enum planck_keycodes {
     COLEMAK
 };
 
+enum {
+    FN_TAP_DANCE = 0
+};
+
 #define _QWERTY 0
 #define _COLEMAK 1
 #define _PROG 2
@@ -23,8 +27,10 @@ enum planck_keycodes {
 #define CTL_ESC     CTL_T(KC_ESC)               // Tap for Esc, hold for Ctrl
 #define HPR_TAB     ALL_T(KC_TAB)               // Tap for Tab, hold for Hyper (Super+Ctrl+Shift+Alt)
 #define SFT_ENT     SFT_T(KC_ENT)               // Tap for Enter, hold for Shift
-
-
+#define MO_MOUS     MO(_MOUSE)                  // Hold for mouse
+#define CPS_FN1    LT(_FN1, KC_CAPS)                  // Hold for mouse
+#define SPC_MOUS   LT(_MOUSE, KC_SPC)                  // Hold for mouse
+#define TAP_FNS    TD(FN_TAP_DANCE)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   /* 0: Qwerty layer
@@ -44,53 +50,50 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QWERTY] = KEYMAP(
     KC_GESC,  KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSLS,  KC_GRV,  \
     HPR_TAB,  KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,            KC_BSPC,  \
-    KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,                      KC_ENT,  \
-    KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_UP,    KC_RSFT,  KC_RSFT, \
-    KC_LCTL,  KC_LALT,  KC_LGUI,                                KC_SPC,                                 MO(_FN1), KC_LEFT,  KC_DOWN,  KC_RGHT,  MO(_FN1)),
+    CPS_FN1,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,  KC_NO,              KC_ENT,  \
+    KC_LSFT,  KC_NO,    KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_UP,    KC_RSFT,  KC_RSFT, \
+    KC_LCTL,  KC_LALT,  KC_LGUI,                                SPC_MOUS,                               KC_APP,   KC_LEFT,  KC_DOWN,  KC_RGHT,  TAP_FNS),
 
   [_COLEMAK] = KEYMAP(
     KC_GESC,  KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSLS,  KC_GRV,   \
     HPR_TAB,  KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,            KC_BSPC,  \
-    KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,                      KC_ENT,   \
-    KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_UP,    KC_RSFT,  KC_RSFT,  \
-    KC_LCTL,  KC_LALT,  KC_LGUI,                                KC_SPC,                                 MO(_FN1), KC_LEFT,  KC_DOWN,  KC_RGHT,  MO(_FN1)),
+    CPS_FN1,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,  KC_NO,              KC_ENT,   \
+    KC_LSFT,  KC_NO,    KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_UP,    KC_RSFT,  KC_RSFT,  \
+    KC_LCTL,  KC_LALT,  KC_LGUI,                                SPC_MOUS,                               KC_APP,   KC_LEFT,  KC_DOWN,  KC_RGHT,  TAP_FNS),
 
 
   [_PROG] = KEYMAP(
     _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  \
     _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,  \
-    _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,                      _______,  \
-    KC_LSPO,            _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  KC_RSPC,  KC_RSPC,  \
-    _______,  _______,  _______,                                _______,                                _______,  _______,  _______,  _______,  _______),
+    KC_LCTL,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,  \
+    KC_LSPO,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  KC_RSPC,  KC_RSPC,  \
+    MO(_FN1), _______,  _______,                                _______,                                _______,  _______,  _______,  _______,  _______),
 
   [_FN1] = KEYMAP(
     KC_GRV,     KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,   KC_F10,   KC_F11,   KC_F12,  _______,  _______,  \
     _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,             KC_DEL,  \
-    _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,                      _______,  \
-    _______,            _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  KC_PGUP,   _______, _______,  \
-    _______,  _______,  _______,                                _______,                                _______,  KC_HOME,  KC_PGDOWN, KC_END,  _______),
+    _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,  \
+    _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  KC_PGUP,   _______, _______,  \
+    _______,  _______,  _______,                                _______,                                KC_INS,   KC_HOME,  KC_PGDOWN, KC_END,  _______),
 
   [_FN2] = KEYMAP(
-    RESET,    _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  \
+    RESET,    _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  QWERTY,   COLEMAK,  \
     _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,  \
-    _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,                      _______,  \
-    _______,            _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  \
+    _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,  \
+    _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  \
     _______,  _______,  _______,                                _______,                                _______,  _______,  _______,  _______,  _______),
 
   [_MOUSE] = KEYMAP(
     _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  \
     _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,  \
-    _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,                      _______,  \
-    _______,            KC_ACL0,  KC_ACL1,  KC_ACL2,  _______,  _______,  _______,  _______,  _______,  KC_WH_U,  _______,  KC_MS_U,  _______,  _______,  \
-    _______,  _______,  _______,                                KC_BTN1,                                KC_WH_D,  KC_MS_L,  KC_MS_D,  KC_MS_R,  KC_BTN2),
+    _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,  \
+    _______,  _______,  KC_ACL0,  KC_ACL1,  KC_ACL2,  _______,  _______,  _______,  _______,  _______,  KC_WH_U,  _______,  KC_MS_U,  _______,  _______,  \
+    KC_BTN1,  _______,  _______,                                _______,                                KC_WH_D,  KC_MS_L,  KC_MS_D,  KC_MS_R,  KC_BTN2),
 
 };
-KC_MS_BTN1, , KC_MS_BTN2,
 // Custom Actions
 const uint16_t PROGMEM fn_actions[] = {
-    [0] = ACTION_LAYER_MODS(_LS, MOD_LSFT)
-    // ,
-    // [1] = ACTION_LAYER_MODS(_RS, MOD_RSFT),
+    // [0] = ACTION_LAYER_MODS(_LS, MOD_LSFT)
 };
 
 
@@ -116,10 +119,64 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     return true;
 }
 
+enum {
+    SINGLE_TAP = 1,
+    SINGLE_HOLD = 2,
+    DOUBLE_TAP = 3,
+    DOUBLE_HOLD = 4,
+    DOUBLE_SINGLE_TAP = 5 //send SINGLE_TAP twice - NOT DOUBLE_TAP
+    // Add more enums here if you want for triple, quadruple, etc.
+  };
 
 
+typedef struct {
+    bool is_press_action;
+    int state;
+} tap;
 
+int cur_dance (qk_tap_dance_state_t *state) {
+    if (state->count == 1) {
+      //If count = 1, and it has been interrupted - it doesn't matter if it is pressed or not: Send SINGLE_TAP
+      if (state->interrupted || !state->pressed) return SINGLE_TAP;
+      else return SINGLE_HOLD;
+    }
+    //If count = 2, and it has been interrupted - assume that user is trying to type the letter associated
+    //with single tap. In example below, that means to send `xx` instead of `Escape`.
+    else if (state->count > 1) {
+      if (state->interrupted) return DOUBLE_SINGLE_TAP;
+      else if (state->pressed) return DOUBLE_HOLD;
+      else return DOUBLE_TAP;
+    }
+    else return 6; //magic number. At some point this method will expand to work for more presses
+  }
 
+// instanalize an instance of 'tap' for the 'FN' tap dance.
+static tap fn_tap_state = {
+    .is_press_action = true,
+    .state = 0
+};
+
+void fn_finished (qk_tap_dance_state_t *state, void *user_data) {
+    fn_tap_state.state = cur_dance(state);
+    switch (fn_tap_state.state) {
+      case SINGLE_HOLD: layer_on(_FN1); break;
+      case DOUBLE_HOLD: layer_on(_FN2); break;
+    }
+  }
+
+void fn_reset (qk_tap_dance_state_t *state, void *user_data) {
+    switch (fn_tap_state.state) {
+        case SINGLE_HOLD: layer_off(_FN1); break;
+        case DOUBLE_HOLD: layer_off(_FN2);
+    }
+    fn_tap_state.state = 0;
+}
+
+qk_tap_dance_action_t tap_dance_actions[] = {
+    [FN_TAP_DANCE] = ACTION_TAP_DANCE_FN_ADVANCED (NULL, fn_finished, fn_reset)
+};
+
+/*
 
 // // Right Shift Layer
 //   [_RS] = KEYMAP(
@@ -129,7 +186,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 //       KC_CAPS, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, \
 //       ______, ______, ______,                            ______,                        ______, ______, ______, ______, ______),
 
-
+*/
 
 /*
 // Function Layer
